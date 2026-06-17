a9677018205f009acaf7ff6198146ed7178bb12e
katagaki <68231035+katagaki@users.noreply.github.com>
2026-06-17 13:05:06 +0900

    Update studio metadata

--- studio/Package.swift
+++ studio/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "DJDXStudio",
-    platforms: [.macOS(.v14)],
+    platforms: [.macOS("26.0")],
     dependencies: [
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.1.0"),
     ],
